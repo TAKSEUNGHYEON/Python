6a266d263453e98a8cf74b3de6d98926da406212
TAk SEUNG HYEON <tmdgus332576@gmail.com>
2020-12-04 17:14:41 +0900

    Initial commit for python practice

---
+++ 숫자 처리 함수.py
@@ -0,0 +1,19 @@
+#절대값(abs)
+print(abs(-5)) #5
+#(n, m) n의 m 승 (pow)
+print(pow(4, 2)) #4^2 = 4*4 = 16
+#최대값(max)
+print(max(5, 12))
+#최솟값(min) 
+print(min(5, 12))
+#반올림(round)
+print(round(3.14))
+
+from math import *
+
+#내림(floor)
+print(floor(4.99))
+#올림(ceil)
+print(ceil(3.14))
+#제곱근(sqrt)\
+print(sqrt(16))
---
+++ 연산자.py
@@ -0,0 +1,11 @@
+print(1 != 3) # True
+print(not(1 != 3)) #False
+
+
+print((3 > 0) and ( 3 < 5)) #true
+print((3 > 0) & ( 3 < 5)) #true
+
+print(( 3 > 0 ) or ( 3 > 5 ))
+print(( 3 > 0) | ( 3 > 5))
+  
+ 
